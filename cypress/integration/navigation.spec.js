92da6b0fe106a495f08adbe7e3deda0dc79ddf93
Brittini Bryant <brittinitb@gmail.com>
2022-02-07 14:21:01 -0500

    Creating Functions for Category Nav
    Creating functions that navigates through the different categories.

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -8,6 +8,7 @@ context('Navigation Tests', () => {
 
     it('Can Navigate To Each Category', () => {
         visit(URL)
+        categorynav()
     })
 })
 
@@ -18,6 +19,39 @@ function visit(URL) {
 
 //Navigates through each category page
 function categorynav() {
-    
+    //Navigates to Desktops
+    cy.get('.nav > :nth-child(1) > .dropdown-toggle') 
+        .click()
+    cy.get('.open > .dropdown-menu > .see-all')
+        .click()
+    //Navigates to Laptops and Notebooks
+    cy.get('.nav > :nth-child(2) > .dropdown-toggle')
+        .click()
+    cy.get('.open > .dropdown-menu > .see-all')
+        .click()
+    //Navigates to Components
+    cy.get(':nth-child(3) > .dropdown-toggle')
+        .click()
+    cy.get('.open > .dropdown-menu > .see-all')    
+    //Navigates to Tablets
+    cy.get('.nav > :nth-child(4) > a')
+        .click() 
+    //Navigates to Software
+    cy.get('.nav > :nth-child(5) > a')
+        .click()  
+    //Navigates to Phones and PDAs
+    cy.get(':nth-child(6) > a')
+        .click()
+    //Navigates to Cameras
+    cy.get(':nth-child(7) > a')
+        .click()
+    //Navigates to MP3 Players
+    cy.get(':nth-child(8) > .dropdown-toggle')
+        .click()
+    cy.get('.open > .dropdown-menu > .see-all')
+        .click()
+             
+
+
 }
 
